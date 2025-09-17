41931a5022e3b17c026df7b393d4000fafe31905
MembaDavid <davidmemba2@gmail.com>
2025-09-17 23:27:02 +0300

    feat: implement About page with header and footer components

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import AboutPage from "@/components/about";
+
+function About() {
+  return (
+    <main>
+      <Header />
+      <AboutPage />
+      <Footer />
+    </main>
+  );
+}
+
+export default About;
