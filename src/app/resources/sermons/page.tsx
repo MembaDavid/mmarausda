c578f34c8dba28684bfa9e8359308a2d3110d19e
MembaDavid <davidmemba2@gmail.com>
2025-09-19 10:08:24 +0300

    feat: create Sermons page with header and footer components

---
+++ src/app/resources/sermons/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import SermonsPage from "@/components/sermons";
+import Footer from "@/components/footer";
+
+export default function Sermons() {
+  return (
+    <div>
+      <Header />
+      <SermonsPage />
+      <Footer />
+    </div>
+  );
+}
