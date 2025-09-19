41e03394e7939cee1714f197f6a7b1547ea91534
MembaDavid <davidmemba2@gmail.com>
2025-09-19 09:55:12 +0300

    feat: add Tailwind CSS configuration for custom gold color palette

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          300: "#FFD966",
+          400: "#FFC107", // golden yellow
+          500: "#FFB300",
+        },
+      },
+    },
+  },
+};
