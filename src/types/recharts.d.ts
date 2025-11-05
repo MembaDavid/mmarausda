40b4df8f25bab75c3dd63038e41d91c8875d188b
MembaDavid <davidmemba2@gmail.com>
2025-11-05 22:02:52 +0300

    feat: add minimal TypeScript module declaration for recharts

---
+++ src/types/recharts.d.ts
@@ -0,0 +1,3 @@
+// Minimal module declaration to silence TypeScript when `recharts` is not installed in the environment.
+// If you later install `recharts`, you can remove this file.
+declare module "recharts";
